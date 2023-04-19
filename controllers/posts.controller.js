b14a50f66721aacd1923fd7575563e4cd2a3ff9e
minhlong149 <minhlong9514@gmail.com>
2023-04-19 21:57:32 +0700

    Get posts for a users by latest

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -1,68 +1,39 @@
 import Post from '../models/post.model.js';
 import User from '../models/user.model.js';
-import Comment from '../models/comment.model.js';
 
 export default class PostsController {
   static async getPosts(request, response) {
-    // return 200 OK and the response, WITH the likes and comments count !!
     const { caption, hashtag, sortBy } = request.query;
-    //Lấy tất cả các bài viết của bạn bè và của chính mình.
-    const currentUser = await User.findById(request.body.id); 
-    const userPosts = await Post.find({ author: currentUser.id });
-    const friendPosts = await Promise.all(
-      currentUser.friendsList.map((userId) => {
-        return Post.find({ author: userId });
-      }) 
-    );
-    const postsGetDefault = userPosts.concat(...friendPosts);
-    let postsList;
-    switch (sortBy) {
-      // sort for homepage display
-      case 'latest':
-        // sorted by date post
-        postsList = postsGetDefault.sort((a, b) => b.createdAt - a.createdAt);
-        break;
+    const { userID } = request.body;
 
-      case 'popular':
+    try {
+      const currentUser = await User.findById(userID);
+      const userFriendsIds = currentUser.friendsList.map((friend) => friend.userId);
+
+      // TODO: Check if user ID is valid (found in DB)
+
+      switch (sortBy) {
+        case 'latest':
+          const postedByUser = { author: currentUser.id };
+          const postedByUserFriends = { author: { $in: userFriendsIds } };
+
+          const latestPosts = await Post.find()
+            .or([postedByUser, postedByUserFriends])
+            .sort({ createdAt: -1 });
+
+          response.json(latestPosts);
+          break;
+
+        case 'popular':
         // sorted by a scoring system
-        //Tính điểm để sắp xếp theo độ phổ biên
-       const posts = await Promise.all(
-         postsGetDefault.map(async (post) => {
-           const populatedPost = await Post.findById(post.id)
-             .populate('likes')
-             .populate('comments')
-             .populate('shares');
-           return {
-             post: populatedPost,
-             points:
-               populatedPost.likes.length +
-               populatedPost.comments.length * 2 +
-               populatedPost.shares.length * 3,
-           };
-         }),
-       );
-        postsList = posts.sort((a, b) => b.points - a.points);
-       // console.log(postsList);
-        break;
 
-      default:
+        default:
         // don't sort, filter by caption or ONE hashtag for searching
-        if (caption) {
-          postsList = postsGetDefault.filter(post => post.caption.includes(caption));
-        }
-        else if (hashtag) {
-          postsList = postsGetDefault.filter((post) => post.hashtags.includes(hashtag));
-        }
-        else postsList = postsGetDefault;
-        break;
+      
+      }
+    } catch (error) {
+      response.status(500).json({ message: error.message });
     }
-
-   try {
-     response.status(200).json(postsList);
-   } catch (error) {
-     response.status(500).json({ message: error.message });
-     response.json(postsList);
-   }
   }
 
   static async getPostsById(request, response) {
